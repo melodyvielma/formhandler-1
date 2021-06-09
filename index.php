dad6e75efb643d66e71709a269d45813088d5e1d
Melody Vielma <mvielma2001@gmail.com>
2021-06-09 11:34:00 -0700

    adds var dump

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 if(isset($_GET['FirstName'])) { //show data
-	echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
+	//echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
+
+	echo '<pre>';
+	var_dump($_GET);
+	echo '</pre>';
 }
 else { //show form
  echo '
